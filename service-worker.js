12876433b0dea344c17254854ef7d0a15aac93cf
SubhraC-prog <subhrachakraborty049@gmail.com>
2025-12-07 00:42:25 +0530

    Refactor service worker caching logic
    Cache clearing update for newer version

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,35 @@
+const CACHE_NAME = 'moneywise-cache-v2'; 
+
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('moneywise-cache-v2').then((cache) => {
-      return cache.addAll([
-        './index.html',
-        './manifest.json',
-        './icon.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
     })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
     })
   );
 });
