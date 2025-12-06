990978e5e711dd71b507ac5f91149ea69857121f
SubhraC-prog <subhrachakraborty049@gmail.com>
2025-12-07 00:33:43 +0530

    Update cache version from v1 to v2
    Version 2 cache due to update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('moneywise-cache-v1').then((cache) => {
+    caches.open('moneywise-cache-v2').then((cache) => {
       return cache.addAll([
         './index.html',
         './manifest.json',
